0d739065ea9e6901e68595b495b5cc2b1bb5609c
aeinstein <mb@tellimate.de>
2024-07-06 19:15:09 +0200

    create new style

--- modules/styles/style.js
+++ modules/styles/style.js
@@ -1,9 +1,42 @@
+let styles;
+
 function refreshStyleEditor(){
     const style = document.getElementById("style").value;
 
     let style_code = styles[style];
 
     document.getElementById("editor").src = "style_editor.html?S=" + encodeURI(style_code);
+}
+
+function deleteStyle(){
+    const style = document.getElementById("style").value;
+
+    delete styles[style];
+
+    localStorage.setItem("STYLES", JSON.stringify(styles));
+    init();
+}
+
+function newStyle(){
+    showTemplate("tmpNewStyle");
+}
+
+function saveNewStyle(){
+    let name = document.getElementById("StyleName").value;
+    const box = document.getElementById("style");
+
+    const option = document.createElement("OPTION");
+    option.value = name;
+    option.text = name;
+
+    box.appendChild(option);
+    box.value = name;
+
+    styles[name] = "";
+
+    localStorage.setItem("STYLES", JSON.stringify(styles));
+    refreshStyleEditor();
+    hideTemplate();
 }
 
 function init(){
@@ -16,6 +49,7 @@ function init(){
     }
 
     document.getElementById("style").innerHTML = content;
+    refreshStyleEditor();
 }
 
 window.addEventListener("load", init);
